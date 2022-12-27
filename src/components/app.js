ca9a2a405036436fa27520f173e86a2dd22de546
Vladimir Loskutov <vladimirloskutov@icloud.com>
2022-12-28 00:36:05 +0300

    add app.js

---
+++ src/components/app.js
@@ -0,0 +1,16 @@
+function app() {
+    const path = location.pathname;
+    const root = document.getElementById('root');
+    let page;
+
+    switch (path) {
+        case '/':
+            break;
+        default:
+            break;
+    }
+
+    return root.appendChild(page);
+}
+
+export { app };
